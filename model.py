6b96910692a6e429e64e98eb1b4cafaed5d64720
Claire Gregg <clairegregg@clairegregg.com>
2023-12-28 16:39:39 +0000

    Basic q-steps ahead model

---
+++ model.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Ridge
+import matplotlib.pyplot as plt
+import math
+
+def q_steps_ahead_pred(q: int, dd: int, lag: int, plot: bool, df: pd.DataFrame, test_prop: float):
+    stride = 1
+
+    t = df['TIME']
+    y = df['AVAILABLE_BIKE_STANDS']
+    XX = y[0:y.size-q-lag*dd:stride]
+    for i in range(1,lag):
+        X = y[i*dd:y.size-q-(lag-i)*dd:stride]
+        XX = np.column_stack((XX,X))
+
+    yy = y[lag*dd+q::stride].iloc[:]
+    yy = yy.reset_index(drop=True)
+    print(yy)
+    tt = t[lag*dd+q::stride]
+
+    train, test = train_test_split(np.arange(0,yy.size), test_size=test_prop)
+    print(yy[train])
+    model = Ridge(fit_intercept=False).fit(XX[train], yy[train])
+
+    print(f"Intercept: {model.intercept_}, Coefficients: {model.coef_}")
+
+    if plot:
+        y_pred = model.predict(XX)
+        plt.scatter(t,y, color='black', marker='.', label="Actual Data")
+        plt.scatter(tt, y_pred, color='blue', marker='.', label="Predicted")
+        plt.xlabel("Time")
+        plt.ylabel("Bike usage")
+        plt.legend()
+        plt.show()
+
+def read_daily_data() -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
+    pre_pandemic = pd.read_csv("data/daily_pre-pandemic.csv", parse_dates=['TIME'])
+    pandemic = pd.read_csv("data/daily_pandemic.csv", parse_dates=['TIME'])
+    post_pandemic = pd.read_csv("data/daily_post-pandemic.csv", parse_dates=['TIME'])
+    return pre_pandemic, pandemic, post_pandemic
+
+def main():
+    pre_pandemic, pandemic, post_pandemic = read_daily_data()
+    
+    # Prediction using short-term trend
+    q_steps_ahead_pred(q=10,dd=1,lag=3,plot=True, df=pre_pandemic, test_prop=0.2)
+
+    # Prediction using daily seasonality
+    t_full=pd.array(pd.DatetimeIndex(pre_pandemic.iloc[:,1]).astype(np.int64))/1000000000
+    dt = t_full[1]-t_full[0]
+    print(f"Data sampling interval is {dt} seconds")
+
+    d=math.floor(24*60*60/dt)
+    q_steps_ahead_pred(q=d,dd=d, lag=3, plot=True, df=pre_pandemic, test_prop=0.2)
+
+    # Prediction using weekly seasonality
+    d = math.floor(7*24*60*80/dt)
+    q_steps_ahead_pred(q=d,dd=d, lag=3, plot=True, df=pre_pandemic, test_prop=0.2)
+
+    # Prediction using monthly seasonality
+    d= math.floor(4*7*24*60*60/dt)
+    q_steps_ahead_pred(q=d, dd=d, lag=3, plot=True, df=pre_pandemic, test_prop=0.2)
+
+if __name__ == "__main__":
+    main()
