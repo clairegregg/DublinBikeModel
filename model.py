ced28bff0b884808034cb83e3505c181cc6e6c52
Claire Gregg <clairegregg@clairegregg.com>
2023-12-29 11:45:33 +0000

    Attempt at LSTM

--- model.py
+++ model.py
@@ -1,100 +1,66 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Ridge
 import matplotlib.pyplot as plt
-import math
-
-def q_steps_ahead_pred(q: int, dd: int, lag: int, plot: bool, df: pd.DataFrame, test_prop: float):
-    stride = 1
-
-    t = df['TIME']
-    y = df['AVAILABLE_BIKE_STANDS']
-    XX = y[0:y.size-q-lag*dd:stride]
-    for i in range(1,lag):
-        X = y[i*dd:y.size-q-(lag-i)*dd:stride]
-        XX = np.column_stack((XX,X))
-
-    yy = y[lag*dd+q::stride].iloc[:]
-    yy = yy.reset_index(drop=True)
-    print(yy)
-    tt = t[lag*dd+q::stride]
-
-    train, test = train_test_split(np.arange(0,yy.size), test_size=test_prop)
-    print(yy[train])
-    model = Ridge(fit_intercept=False).fit(XX[train], yy[train])
-
-    print(f"Intercept: {model.intercept_}, Coefficients: {model.coef_}")
-
-    if plot:
-        y_pred = model.predict(XX)
-        plt.scatter(t,y, color='black', marker='.', label="Actual Data")
-        plt.scatter(tt, y_pred, color='blue', marker='.', label="Predicted")
-        plt.xlabel("Time")
-        plt.ylabel("Bike usage")
-        plt.legend()
-        plt.show()
-
-def combined_q_steps_ahead_pred(q: int, lag: int, plot: bool, df1: pd.DataFrame, df2: pd.DataFrame, test_prop: float, dt: float):
-    stride=1
-
-    df = pd.concat([df1,df2])
-    
-    w = math.floor(4*7*24*60*60/dt)
-    t = df['TIME']
-    y = df['AVAILABLE_BIKE_STANDS']
-    length = y.size-w-lag * w-q
-    
-    XX = y[q:q+length:stride]
-
-    for i in range(1, lag):
-        X=y[i*w+q:i*w+q+length:stride]
-        XX= np.column_stack((XX,X))
-
-    d = math.floor(7*24*60*60/dt)
-    for i in range(0,lag):
-        X=y[i*d+q:i*d+q+length:stride]
-        XX=np.column_stack((XX,X))
-
-    d = math.floor(24*60*60/dt)
-    for i in range(0,lag):
-        X=y[i*d+q:i*d+q+length:stride]
-        XX=np.column_stack((XX,X))
-
-    for i in range(0,lag):
-        X=y[i:i+length:stride]
-        XX=np.column_stack((XX,X))
-
-    yy=y[lag*w+w+q:lag*w+w+q+length:stride]
-    yy = yy.reset_index(drop=True)
-    tt=t[lag*w+w+q:lag*w+w+q+length:stride]
-
-    train,test = train_test_split(np.arange(0,yy.size), test_size=test_prop)
-    model = Ridge(fit_intercept=False).fit(XX[train], yy[train])
-    print(f"Intercept is {model.intercept_}, coefficient is {model.coef_}")
-
-    if plot:
-        y_pred = model.predict(XX)
-        plt.scatter(t,y, color='black', marker='.', label="Actual Data")
-        plt.scatter(tt, y_pred, color='blue', marker='.', label="Predicted")
-        plt.xlabel("Time")
-        plt.ylabel("Bike usage")
-        plt.legend()
-        plt.show()
-
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
 
 def read_daily_data() -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
-    pre_pandemic = pd.read_csv("data/daily_pre-pandemic.csv", parse_dates=['TIME'])
-    pandemic = pd.read_csv("data/daily_pandemic.csv", parse_dates=['TIME'])
-    post_pandemic = pd.read_csv("data/daily_post-pandemic.csv", parse_dates=['TIME'])
+    columns = ["TIME","AVAILABLE_BIKE_STANDS"]
+    pre_pandemic = pd.read_csv("data/daily_pre-pandemic.csv", parse_dates=['TIME'], usecols=columns)
+    pandemic = pd.read_csv("data/daily_pandemic.csv", parse_dates=['TIME'], usecols=columns)
+    post_pandemic = pd.read_csv("data/daily_post-pandemic.csv", parse_dates=['TIME'], usecols=columns)
     return pre_pandemic, pandemic, post_pandemic
 
-def main():
-    pre_pandemic, pandemic, post_pandemic = read_daily_data()
-    t_full=pd.array(pd.DatetimeIndex(pre_pandemic.iloc[:,1]).astype(np.int64))/1000000000
-    dt = t_full[1]-t_full[0]
+def new_lstm_dataset(dataset: [float], look_back: int = 1) -> np.ndarray:
+    dataX, dataY = [], []
+    for i in range(len(dataset)-look_back-1):
+        dataX.append(dataset[i:(i+look_back),0])
+        dataY.append(dataset[i+look_back, 0])
+    return np.array(dataX), np.array(dataY)
 
-    combined_q_steps_ahead_pred(10, 3, True, pre_pandemic, pandemic, 0.2, dt)
+def test_accuracy_lstm(dataset: [float], train_proportion: float):
+    # Setup train test split
+    train_size = int(len(dataset) * train_proportion)
+    test_size = len(dataset) - train_size
+    train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+
+    # Create features as one time step
+    look_back=1
+    trainX, trainY = new_lstm_dataset(train, look_back)
+    testX, testY = new_lstm_dataset(test, look_back)
+
+    # Reshape input to be [sample, time step, feature]
+    trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+    testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+
+    # Create and fit the LSTM network
+    model = Sequential()
+    model.add(LSTM(4, input_shape=(1, look_back)))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+
+    return
+
+def main():
+    # Ensure it is reproducable
+    tf.random.set_seed(42)
+
+    # Read data in
+    pre_pandemic, pandemic, post_pandemic = read_daily_data()
+    pre_pandemic_dataset = (pre_pandemic.drop(columns=['TIME'])).values
+    pandemic_dataset = (pandemic.drop(columns=['TIME'])).values
+    post_pandemic_dataset = (post_pandemic.drop(columns=['TIME'])).values
+
+    # Normalise the datasets
+    scaler = MinMaxScaler(feature_range=(0,1))
+    pre_pandemic = scaler.fit_transform(pre_pandemic_dataset)
+    pandemic = scaler.fit_transform(pandemic_dataset)
+    post_pandemic = scaler.fit_transform(post_pandemic_dataset)
+    
 
 if __name__ == "__main__":
     main()
