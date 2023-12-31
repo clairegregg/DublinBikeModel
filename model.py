d396f5a9702c2fa3690dcb9201c9497fa0e94c19
Claire Gregg <clairegregg@clairegregg.com>
2023-12-31 14:33:35 +0000

    LSTM 1-step
    From https://machinelearningmastery.com/time-series-forecasting-long-short-term-memory-network-python/

--- model.py
+++ model.py
@@ -7,6 +7,8 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
+from math import sqrt, floor
+from tqdm import tqdm
 
 def read_daily_data() -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
     columns = ["TIME","AVAILABLE_BIKE_STANDS"]
@@ -15,64 +17,83 @@ def read_daily_data() -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
     post_pandemic = pd.read_csv("data/daily_post-pandemic.csv", parse_dates=['TIME'], usecols=columns)
     return pre_pandemic, pandemic, post_pandemic
 
-def new_lstm_dataset(dataset: [float], look_back: int = 1) -> np.ndarray:
-    dataX, dataY = [], []
-    for i in range(len(dataset)-look_back-1):
-        dataX.append(dataset[i:(i+look_back),0])
-        dataY.append(dataset[i+look_back, 0])
-    return np.array(dataX), np.array(dataY)
+def baseline_model(train: [float], test: [float]):
+    history = [x for x in train]
+    predictions = []
 
-def test_accuracy_lstm(dataset: [float], train_proportion: float, scaler: MinMaxScaler):
-    # Setup train test split
-    train_size = int(len(dataset) * train_proportion)
-    train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+    for i in range (len(test)):
+        # Make prediction
+        predictions.append(history[-1])
 
-    # Create features as one time step
-    look_back=1
-    trainX, trainY = new_lstm_dataset(train, look_back)
-    testX, testY = new_lstm_dataset(test, look_back)
+        # New observation
+        history.append(test[i])
+    
+    # Report performance
+    error = sqrt(mean_squared_error(test, predictions))
+    print("Baseline model mean squared error: %.2f"%error)
 
-    # Reshape input to be [sample, time step, feature]
-    trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
-    testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
-
-    # Create and fit the LSTM network
-    model = Sequential()
-    model.add(LSTM(4, input_shape=(1, look_back)))
-    model.add(Dense(1))
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
-
-    # Make predictions
-    trainPredict = model.predict(trainX)
-    testPredict = model.predict(testX)
-
-    # Invert predictions
-    trainPredict = scaler.inverse_transform(trainPredict)
-    trainY = scaler.inverse_transform([trainY])
-    testPredict = scaler.inverse_transform(testPredict)
-    testY = scaler.inverse_transform([testY])
-
-    # Calculate error
-    trainScore = np.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
-    print("Train score: %.2f mean squared error"%(trainScore))
-    testScore = np.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
-    print("Test score: %.2f mean squared error"%(testScore))
-
-    # Shift predictions for plotting
-    trainPredictPlot = np.empty_like(dataset)
-    trainPredictPlot[:,:] = np.nan
-    trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
-
-    testPredictPlot = np.empty_like(dataset)
-    testPredictPlot[:,:] = np.nan
-    testPredictPlot[len(testPredict)+(look_back*2)+1:len(dataset)-1,:] = testPredict
-
-    # Plot baseline model and predictions
-    plt.plot(scaler.inverse_transform(dataset))
-    plt.plot(trainPredictPlot)
-    plt.plot(testPredictPlot)
+    # Plot observed vs predicted
+    plt.plot(test)
+    plt.plot(predictions)
     plt.show()
+
+def difference_series(dataset: [float], interval: int=1) -> pd.Series:
+    diff = list()
+    for i in tqdm(range(interval, len(dataset)), desc="Making dataset stationary"):
+        value = dataset[i] - dataset[i-interval]
+        diff.append(value)
+    return pd.Series(diff)
+
+def inverse_difference_series(history, yhat, interval=1):
+ return yhat + history[-interval]
+
+def series_to_input_output(data: [float], lag: int=1) -> pd.DataFrame:
+    df = pd.DataFrame(data)
+    columns = [df.shift(i) for i in range(1, lag+1)]
+    columns.append(df)
+    df = pd.concat(columns, axis=1)
+    df.fillna(0,inplace=True)
+    return df
+
+def scale(train, test):
+    scaler = MinMaxScaler(feature_range=(-1,1))
+    scaler.fit(train)
+
+    train = train.reshape(train.shape[0], train.shape[1])
+    train_scaled = scaler.transform(train)
+
+    test = test.reshape(test.shape[0], test.shape[1])
+    test_scaled = scaler.transform(test)
+
+    return scaler, train_scaled, test_scaled
+
+def invert_scale(scaler, X, value):
+    new_row = [x for x in X] + [value]
+    array = np.array(new_row)
+    array = array.reshape(1, len(array))
+    inverted = scaler.inverse_transform(array)
+    return inverted[0,-1]
+
+def fit_lstm(train, batch_size, nb_epoch, neurons):
+    X, y = train[:, 0:-1], train[:, -1]
+    X = X.reshape(X.shape[0], 1, X.shape[1])
+
+    model = Sequential()
+    model.add(LSTM(neurons, batch_input_shape=(batch_size, X.shape[1], X.shape[2]), stateful=True))
+    model.add(Dense(1))
+    model.compile(loss="mean_squared_error", optimizer='adam')
+    
+    for _ in tqdm(range(nb_epoch), desc="Fitting model"):
+        model.fit(X, y, epochs=1, batch_size=batch_size, verbose=0, shuffle=False)
+        model.reset_states()
+    return model
+
+def forecast(model, batch_size, row):
+    X = row[0:-1]
+    X = X.reshape(1, 1, len(X))
+    yhat = model.predicted(X, batch_size=batch_size)
+    return yhat[0,0]
+
 
 def main():
     # Ensure it is reproducable
@@ -80,18 +101,52 @@ def main():
 
     # Read data in
     pre_pandemic, pandemic, post_pandemic = read_daily_data()
+    print(pre_pandemic)
     pre_pandemic_dataset = (pre_pandemic.drop(columns=['TIME'])).values
-    pandemic_dataset = (pandemic.drop(columns=['TIME'])).values
-    post_pandemic_dataset = (post_pandemic.drop(columns=['TIME'])).values
 
-    # Normalise the datasets
-    scaler = MinMaxScaler(feature_range=(0,1))
-    pre_pandemic = scaler.fit_transform(pre_pandemic_dataset)
-    pandemic = scaler.fit_transform(pandemic_dataset)
-    post_pandemic = scaler.fit_transform(post_pandemic_dataset)
+    # Transform to be stationary
+    pre_pandemic_diff = difference_series(pre_pandemic_dataset, 1)
 
-    # Test LSTM for this problem
-    test_accuracy_lstm(pre_pandemic, 0.2, scaler)
+    # Transform to supervised learning (input/output)
+    pre_pandemic_supervised = series_to_input_output(pre_pandemic_diff).values
+
+    # Split into train/test
+    train_size = floor(0.8* len(pre_pandemic_supervised))
+    train, test = pre_pandemic_supervised[0:train_size], pre_pandemic_supervised[train_size:]
+
+    # Normalise the data
+    scaler, train_scaled, test_scaled = scale(train, test)
+
+    # Fit the model
+    model = fit_lstm(train_scaled, 1, 3000, 4)
+
+    # Forecast the full training dataset to build up state for forecasting
+    train_reshaped = train_scaled[:,0].reshape(len(train_scaled), 1, 1)
+    model.predict(train_reshaped, batch_size=1)
+
+    # walk-forward validation on the test data
+    predictions = list()
+    for i in tqdm(range(len(test_scaled)), desc=f"Making predictions"):
+        # make one-step forecast
+        X, y = test_scaled[i, 0:-1], test_scaled[i, -1]
+        yhat = forecast(model, 1, X)
+        # invert scaling
+        yhat = invert_scale(scaler, X, yhat)
+        # invert differencing
+        yhat = inverse_difference_series(pre_pandemic_dataset, yhat, len(test_scaled)+1-i)
+        # store forecast
+        predictions.append(yhat)
+        expected = pre_pandemic_dataset[len(train) + i + 1]
+    print('Month=%d, Predicted=%f, Expected=%f' % (i+1, yhat, expected))
+    
+    # report performance
+    rmse = sqrt(mean_squared_error(pre_pandemic_dataset[train_size:], predictions))
+    print('Test RMSE: %.3f' % rmse)
+    # line plot of observed vs predicted
+    plt.plot(pre_pandemic_dataset[train_size:])
+    plt.plot(predictions)
+    plt.show()
+
     
 
 if __name__ == "__main__":
