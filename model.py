c00d9133f1ca82c5b3a3e089b4aef19f2093656c
Claire Gregg <clairegregg@clairegregg.com>
2023-12-31 14:55:51 +0000

    Return back to q-steps ahead model

--- model.py
+++ model.py
@@ -1,153 +1,100 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Ridge
 import matplotlib.pyplot as plt
-import tensorflow as tf
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
-from math import sqrt, floor
-from tqdm import tqdm
+import math
+
+def q_steps_ahead_pred(q: int, dd: int, lag: int, plot: bool, df: pd.DataFrame, test_prop: float):
+    stride = 1
+
+    t = df['TIME']
+    y = df['AVAILABLE_BIKE_STANDS']
+    XX = y[0:y.size-q-lag*dd:stride]
+    for i in range(1,lag):
+        X = y[i*dd:y.size-q-(lag-i)*dd:stride]
+        XX = np.column_stack((XX,X))
+
+    yy = y[lag*dd+q::stride].iloc[:]
+    yy = yy.reset_index(drop=True)
+    print(yy)
+    tt = t[lag*dd+q::stride]
+
+    train, test = train_test_split(np.arange(0,yy.size), test_size=test_prop)
+    print(yy[train])
+    model = Ridge(fit_intercept=False).fit(XX[train], yy[train])
+
+    print(f"Intercept: {model.intercept_}, Coefficients: {model.coef_}")
+
+    if plot:
+        y_pred = model.predict(XX)
+        plt.scatter(t,y, color='black', marker='.', label="Actual Data")
+        plt.scatter(tt, y_pred, color='blue', marker='.', label="Predicted")
+        plt.xlabel("Time")
+        plt.ylabel("Bike usage")
+        plt.legend()
+        plt.show()
+
+def combined_q_steps_ahead_pred(q: int, lag: int, plot: bool, df1: pd.DataFrame, df2: pd.DataFrame, test_prop: float, dt: float):
+    stride=1
+
+    df = pd.concat([df1,df2])
+    
+    w = math.floor(4*7*24*60*60/dt)
+    t = df['TIME']
+    y = df['AVAILABLE_BIKE_STANDS']
+    length = y.size-w-lag * w-q
+    
+    XX = y[q:q+length:stride]
+
+    for i in range(1, lag):
+        X=y[i*w+q:i*w+q+length:stride]
+        XX= np.column_stack((XX,X))
+
+    d = math.floor(7*24*60*60/dt)
+    for i in range(0,lag):
+        X=y[i*d+q:i*d+q+length:stride]
+        XX=np.column_stack((XX,X))
+
+    d = math.floor(24*60*60/dt)
+    for i in range(0,lag):
+        X=y[i*d+q:i*d+q+length:stride]
+        XX=np.column_stack((XX,X))
+
+    for i in range(0,lag):
+        X=y[i:i+length:stride]
+        XX=np.column_stack((XX,X))
+
+    yy=y[lag*w+w+q:lag*w+w+q+length:stride]
+    yy = yy.reset_index(drop=True)
+    tt=t[lag*w+w+q:lag*w+w+q+length:stride]
+
+    train,test = train_test_split(np.arange(0,yy.size), test_size=test_prop)
+    model = Ridge(fit_intercept=False).fit(XX[train], yy[train])
+    print(f"Intercept is {model.intercept_}, coefficient is {model.coef_}")
+
+    if plot:
+        y_pred = model.predict(XX)
+        plt.scatter(t,y, color='black', marker='.', label="Actual Data")
+        plt.scatter(tt, y_pred, color='blue', marker='.', label="Predicted")
+        plt.xlabel("Time")
+        plt.ylabel("Bike usage")
+        plt.legend()
+        plt.show()
+
 
 def read_daily_data() -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
-    columns = ["TIME","AVAILABLE_BIKE_STANDS"]
-    pre_pandemic = pd.read_csv("data/daily_pre-pandemic.csv", parse_dates=['TIME'], usecols=columns)
-    pandemic = pd.read_csv("data/daily_pandemic.csv", parse_dates=['TIME'], usecols=columns)
-    post_pandemic = pd.read_csv("data/daily_post-pandemic.csv", parse_dates=['TIME'], usecols=columns)
+    pre_pandemic = pd.read_csv("data/daily_pre-pandemic.csv", parse_dates=['TIME'])
+    pandemic = pd.read_csv("data/daily_pandemic.csv", parse_dates=['TIME'])
+    post_pandemic = pd.read_csv("data/daily_post-pandemic.csv", parse_dates=['TIME'])
     return pre_pandemic, pandemic, post_pandemic
 
-def baseline_model(train: [float], test: [float]):
-    history = [x for x in train]
-    predictions = []
-
-    for i in range (len(test)):
-        # Make prediction
-        predictions.append(history[-1])
-
-        # New observation
-        history.append(test[i])
-    
-    # Report performance
-    error = sqrt(mean_squared_error(test, predictions))
-    print("Baseline model mean squared error: %.2f"%error)
-
-    # Plot observed vs predicted
-    plt.plot(test)
-    plt.plot(predictions)
-    plt.show()
-
-def difference_series(dataset: [float], interval: int=1) -> pd.Series:
-    diff = list()
-    for i in tqdm(range(interval, len(dataset)), desc="Making dataset stationary"):
-        value = dataset[i] - dataset[i-interval]
-        diff.append(value)
-    return pd.Series(diff)
-
-def inverse_difference_series(history, yhat, interval=1):
- return yhat + history[-interval]
-
-def series_to_input_output(data: [float], lag: int=1) -> pd.DataFrame:
-    df = pd.DataFrame(data)
-    columns = [df.shift(i) for i in range(1, lag+1)]
-    columns.append(df)
-    df = pd.concat(columns, axis=1)
-    df.fillna(0,inplace=True)
-    return df
-
-def scale(train, test):
-    scaler = MinMaxScaler(feature_range=(-1,1))
-    scaler.fit(train)
-
-    train = train.reshape(train.shape[0], train.shape[1])
-    train_scaled = scaler.transform(train)
-
-    test = test.reshape(test.shape[0], test.shape[1])
-    test_scaled = scaler.transform(test)
-
-    return scaler, train_scaled, test_scaled
-
-def invert_scale(scaler, X, value):
-    new_row = [x for x in X] + [value]
-    array = np.array(new_row)
-    array = array.reshape(1, len(array))
-    inverted = scaler.inverse_transform(array)
-    return inverted[0,-1]
-
-def fit_lstm(train, batch_size, nb_epoch, neurons):
-    X, y = train[:, 0:-1], train[:, -1]
-    X = X.reshape(X.shape[0], 1, X.shape[1])
-
-    model = Sequential()
-    model.add(LSTM(neurons, batch_input_shape=(batch_size, X.shape[1], X.shape[2]), stateful=True))
-    model.add(Dense(1))
-    model.compile(loss="mean_squared_error", optimizer='adam')
-    
-    for _ in tqdm(range(nb_epoch), desc="Fitting model"):
-        model.fit(X, y, epochs=1, batch_size=batch_size, verbose=0, shuffle=False)
-        model.reset_states()
-    return model
-
-def forecast(model, batch_size, row):
-    X = row[0:-1]
-    X = X.reshape(1, 1, len(X))
-    yhat = model.predicted(X, batch_size=batch_size)
-    return yhat[0,0]
-
-
 def main():
-    # Ensure it is reproducable
-    tf.random.set_seed(42)
-
-    # Read data in
     pre_pandemic, pandemic, post_pandemic = read_daily_data()
-    print(pre_pandemic)
-    pre_pandemic_dataset = (pre_pandemic.drop(columns=['TIME'])).values
+    t_full=pd.array(pd.DatetimeIndex(pre_pandemic.iloc[:,1]).astype(np.int64))/1000000000
+    dt = t_full[1]-t_full[0]
 
-    # Transform to be stationary
-    pre_pandemic_diff = difference_series(pre_pandemic_dataset, 1)
-
-    # Transform to supervised learning (input/output)
-    pre_pandemic_supervised = series_to_input_output(pre_pandemic_diff).values
-
-    # Split into train/test
-    train_size = floor(0.8* len(pre_pandemic_supervised))
-    train, test = pre_pandemic_supervised[0:train_size], pre_pandemic_supervised[train_size:]
-
-    # Normalise the data
-    scaler, train_scaled, test_scaled = scale(train, test)
-
-    # Fit the model
-    model = fit_lstm(train_scaled, 1, 3000, 4)
-
-    # Forecast the full training dataset to build up state for forecasting
-    train_reshaped = train_scaled[:,0].reshape(len(train_scaled), 1, 1)
-    model.predict(train_reshaped, batch_size=1)
-
-    # walk-forward validation on the test data
-    predictions = list()
-    for i in tqdm(range(len(test_scaled)), desc=f"Making predictions"):
-        # make one-step forecast
-        X, y = test_scaled[i, 0:-1], test_scaled[i, -1]
-        yhat = forecast(model, 1, X)
-        # invert scaling
-        yhat = invert_scale(scaler, X, yhat)
-        # invert differencing
-        yhat = inverse_difference_series(pre_pandemic_dataset, yhat, len(test_scaled)+1-i)
-        # store forecast
-        predictions.append(yhat)
-        expected = pre_pandemic_dataset[len(train) + i + 1]
-    print('Month=%d, Predicted=%f, Expected=%f' % (i+1, yhat, expected))
-    
-    # report performance
-    rmse = sqrt(mean_squared_error(pre_pandemic_dataset[train_size:], predictions))
-    print('Test RMSE: %.3f' % rmse)
-    # line plot of observed vs predicted
-    plt.plot(pre_pandemic_dataset[train_size:])
-    plt.plot(predictions)
-    plt.show()
-
-    
+    combined_q_steps_ahead_pred(10, 3, True, pre_pandemic, pandemic, 0.2, dt)
 
 if __name__ == "__main__":
     main()
