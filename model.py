07f39ffe8b6df4db0d41c41c3f6bac35a5aec8d2
Claire Gregg <clairegregg@clairegregg.com>
2024-01-02 16:52:57 +0000

    Cross-validation

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import Ridge
 import matplotlib.pyplot as plt
 import math
 
-def one_step_ahead(y: pd.Series, lag: int, dt: float) -> float:
+def one_step_ahead(y: pd.Series, lag: int, dt: float, alpha: float) -> float:
     stride, q = 1, 1
 
     # Number of samples in a month
@@ -41,22 +41,20 @@ def one_step_ahead(y: pd.Series, lag: int, dt: float) -> float:
     yy=y[lag*w+w+q:lag*w+w+q+length:stride]
     yy = yy.reset_index(drop=True)
 
-    model = Ridge(fit_intercept=False).fit(XX, yy)
+    model = Ridge(fit_intercept=False, alpha=alpha).fit(XX, yy)
 
     return model.predict(XX)[-1]
 
-def multi_step_prediction(train: pd.DataFrame, test: pd.DataFrame, lag: int, dt: float, plot: bool, error: bool):
+def multi_step_prediction(train: pd.DataFrame, test: pd.DataFrame, lag: int, dt: float, alpha: float, plot: bool):
     y = train.copy()['AVAILABLE_BIKE_STANDS']
     for _ in range(len(test)):
-        new_y = one_step_ahead(y, lag, dt)
+        new_y = one_step_ahead(y, lag, dt, alpha)
         y[len(y)] = new_y
 
     y_pred = y[len(train):]
     y_true = test['AVAILABLE_BIKE_STANDS']
 
-    if error:
-        mse = mean_squared_error(y_true, y_pred)
-        print(f"This model has a mean squared error of {mse}")
+    mse = mean_squared_error(y_true, y_pred)
 
     if plot:
         full_x = pd.concat([train['TIME'], test['TIME']])
@@ -67,6 +65,31 @@ def multi_step_prediction(train: pd.DataFrame, test: pd.DataFrame, lag: int, dt:
         plt.ylabel("Bike usage")
         plt.legend()
         plt.show()
+
+    return mse
+
+def cross_validation_on_pre_pandemic(pre_pandemic: pd.DataFrame, dt: float):
+    c_vals = [0.0001, 0.001, 0.01, 0.1, 1, 10]
+    lags = np.arange(start=1, stop=15)
+    current_min_error = np.inf
+    current_min_c_val, current_min_lag = 0, 0
+
+    train_size = math.floor(0.8*(pre_pandemic.shape[0]))
+    train, test = pre_pandemic[0:train_size], pre_pandemic[train_size:]
+
+    for c in c_vals:
+        for lag in lags:
+            alpha = 1/(2*c)
+            err = multi_step_prediction(train, test, lag, dt, alpha, False)
+            print("checked 1")
+            if err < current_min_error:
+                current_min_error = err
+                current_min_c_val = c
+                current_min_lag = lag
+
+    print(f"Lowest mean squared error with c={current_min_c_val} and lag={current_min_lag}. Error is {current_min_error}.")
+    multi_step_prediction(train, test, current_min_lag, dt, 1/(2*c), True)
+
 
 def read_daily_data() -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
     pre_pandemic = pd.read_csv("data/daily_pre-pandemic.csv", parse_dates=['TIME'])
@@ -79,10 +102,7 @@ def main():
     t_full=pd.array(pd.DatetimeIndex(pre_pandemic.iloc[:,1]).astype(np.int64))/1000000000
     dt = t_full[1]-t_full[0]
 
-    train_size = math.floor(0.8*(pre_pandemic.shape[0]))
-    train, test = pre_pandemic[0:train_size], pre_pandemic[train_size:]
-    
-    multi_step_prediction(train, test, 3, dt, True, True)
+    cross_validation_on_pre_pandemic(pre_pandemic, dt)
 
 if __name__ == "__main__":
     main()
